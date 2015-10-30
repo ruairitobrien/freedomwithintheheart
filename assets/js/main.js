19ac472b6571fdbb85b7eaad9e6a7aaa95d07b9c
Ruairi O Brien <ruairitobrien@gmail.com>
2015-10-30 23:46:49 +0000

    Added intro music

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,8 @@
-jQuery(document).ready(function(){
+jQuery(document).ready(function () {
+	var introMusic = document.getElementById("intro-music");
+	var muteButton = jQuery("#mutebtn");
+	var muteIcon = jQuery("#mutebtn > i");
+	
 	jQuery('#topnav').localScroll(3000);
 	jQuery('#startbtn').localScroll(5000);
 	jQuery('#novelbtn').localScroll(7000);
@@ -13,9 +17,20 @@ jQuery(document).ready(function(){
 	jQuery('#maron-pictures').parallax("50%", 0.1);
 	
 	//hide menu after click on mobile
-	jQuery('.navbar .nav > li > a').click(function(){
+	jQuery('.navbar .nav > li > a').click(function () {
 		jQuery('.navbar-collapse.navbar-ex1-collapse.in').removeClass('in').addClass('collapse').css('height', '0');
 	});
-	
-	jQuery("html").niceScroll({cursorcolor:"#fff"});
+
+	jQuery("html").niceScroll({ cursorcolor: "#fff" });
+
+	muteButton.click(function () {		
+		if(muteIcon.hasClass( "audio-on" )) {
+			muteIcon.addClass('audio-off').addClass('fa-volume-down').removeClass('audio-on').removeClass('fa-volume-up');
+			introMusic.pause();
+		} else {
+			muteIcon.addClass('audio-on').addClass('fa-volume-up').removeClass('audio-off').removeClass('fa-volume-down');
+			introMusic.play();
+		}		
+	});
+
 });
